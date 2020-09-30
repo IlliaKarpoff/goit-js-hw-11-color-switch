01e02929960921a963b37c40427e1aee1a8de182
Illia Karpoff <hawaii2001@ukr.net>
2020-09-30 19:54:51 +0300

    well done

--- src/js/task-2.js
+++ src/js/task-2.js
@@ -107,23 +107,20 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 const makeTransaction = transaction => {
-  const promise = new Promise((resolve, reject) => {
-    const delay = randomIntegerFromInterval(200, 500);
+  const id = transaction.id;
+  const time = randomIntegerFromInterval(200, 500);
+  
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
-      if (canProcess) {
-        resolve(transaction.id, delay);
-      } else {
-        reject(transaction.id);
-      }
-    }, delay);
+      canProcess ? resolve({id, time}) : reject(id);
+    }, time);
   });
-  return promise;
 };
-const logSuccess = (id, time) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = ({id, time}) => {
+  console.log(`Transaction ${id} processed in ${time} ms`);
 };
-const logError = id => {
+const logError = (id) => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 //  Должно работать так
